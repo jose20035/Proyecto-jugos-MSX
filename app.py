e5d4664f46ce1a0894af91bfc94d9c6245b36f40
José Miguel Calderon Frutos <josemiguelcalderonfrutos@gmail.com>
2020-05-02 13:25:15 +0200

    Modificacion del app

--- app.py
+++ app.py
@@ -2,22 +2,14 @@ from flask import Flask, render_template,abort
 import json
 import os
 
-with open('books.json') as file:
+with open('MSX.json') as file:
     libros=json.load(file)
 
 app = Flask(__name__)
 
-@app.route('/categoria/<categoria>')
-@app.route('/')
-def inicio(categoria=" "):
+@app.route('/juegos', methods=["GET","POST"])
+def inicio():
     return render_template("Inicio.html",datos=libros,categoria=categoria)
-
-@app.route('/libro/<isbn>')
-def detail(isbn):
-    for libro in libros:
-        if isbn == libro.get("isbn"):
-            return render_template("Detail.html",libro=libro)
-    abort(404)
 
 port=os.environ["PORT"]
 app.run('0.0.0.0',int(port),debug=False)
