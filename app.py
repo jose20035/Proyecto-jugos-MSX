c14cc212fc5212c1dc7c05518bbd8ddc91cc2bd2
José Miguel Calderon Frutos <josemiguelcalderonfrutos@gmail.com>
2020-05-03 13:18:26 +0200

    Creacion de pagina de juegos

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template,abort
+from flask import Flask, render_template,abort,request
 import json
 import os
 
 with open('MSX.json') as file:
-    libros=json.load(file)
+    juegos=json.load(file)
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET","POST"])
 def inicio():
-    return render_template("base.html")
+    if request.method=="GET":
+        met=1
+        return render_template("juegos.html" met=met)
+    else:
+        busq=request.form.get("busqueda")
+        for 
 
 port=os.environ["PORT"]
 app.run('0.0.0.0',int(port),debug=False)
