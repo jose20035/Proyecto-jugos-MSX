0ecb17d2c43566ea6794b3e5bfd0d0b3135fdd0f
José Miguel Calderon Frutos <josemiguelcalderonfrutos@gmail.com>
2020-05-03 13:36:24 +0200

    modificacion app para juegos

--- app.py
+++ app.py
@@ -13,8 +13,17 @@ def inicio():
         met=1
         return render_template("juegos.html" met=met)
     else:
+        met=0
+        respuesta=[]
         busq=request.form.get("busqueda")
-        for 
+        if busq = "":
+            return render_template("juegos.html" met=met lista=juegos)
+        else:
+            for juego in juegos:
+                if juegos.get("nombre").startswith(busq):
+                    respuesta.append(juego)
+            return render_template("juegos.html" met=met lista=respuesta)
+            
 
 port=os.environ["PORT"]
 app.run('0.0.0.0',int(port),debug=False)
