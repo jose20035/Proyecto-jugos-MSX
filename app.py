58f155cb1e91cbfeb52e6d8c4405d29b3419de80
José Miguel Calderon Frutos <josemiguelcalderonfrutos@gmail.com>
2020-05-03 18:13:53 +0200

    Finalicacion buscador de juegos por nombre

--- app.py
+++ app.py
@@ -5,25 +5,29 @@ import os
 with open('MSX.json') as file:
     juegos=json.load(file)
 
+cate=[]
+for juego in juegos:
+    if juego.get("categoria") not in cate:
+        cate.append(juego.get("categoria"))
+
 app = Flask(__name__)
 
 @app.route('/', methods=["GET","POST"])
 def inicio():
     if request.method=="GET":
         met=1
-        return render_template("juegos.html" met=met)
+        return render_template("juegos.html",met=met,cate=cate)
     else:
-        met=0
         respuesta=[]
         busq=request.form.get("busqueda")
-        if busq = "":
-            return render_template("juegos.html" met=met lista=juegos)
+        if busq == "":
+            return render_template("juegos.html",lista=juegos)
         else:
             for juego in juegos:
-                if juegos.get("nombre").startswith(busq):
+                if str(juego.get("nombre")).startswith(busq):
                     respuesta.append(juego)
-            return render_template("juegos.html" met=met lista=respuesta)
+            return render_template("juegos.html",lista=respuesta)
             
 
 port=os.environ["PORT"]
-app.run('0.0.0.0',int(port),debug=False)
+app.run('0.0.0.0',int(port),debug=True)
